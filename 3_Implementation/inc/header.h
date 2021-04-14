3a3f93cfb0ee6c8772a0a33c1eab4b1da2eddcfb
260007 <81068076+260007@users.noreply.github.com>
2021-04-14 22:29:28 +0530

    Add files via upload

---
+++ 3_Implementation/inc/header.h
@@ -0,0 +1,15 @@
+#ifndef __HEADER_H__
+#define __HEADER_H__
+#define PI 3.1410
+float circle(float,int);
+float triangle(float,float,float,int);
+float square(float,int);
+float rhombus(float,float,int);
+float rectangle(float,float,int);
+float parallelogram(float,float,float,int);
+float trapezium(float,float,float,float,float,int);
+float cube(float,int);
+float cuboid(float,float,float,float,int);
+float cone(float,float,int);
+float sphere(float,int);
+#endif
